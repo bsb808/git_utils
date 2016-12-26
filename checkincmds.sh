68c662dc1aae93e9022a4d08054fa0589ca43318
Bingham, Brian S <briansbingham@gmail.com>
2016-12-26 14:46:32 -0800

    \"${MSG}\"

--- checkincmds.sh
+++ checkincmds.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-MSG="increment on geonavtransorm"
+MSG="working  geonavtransorm"
 
 CMDS=('git commit -a -m \"${MSG}\" ' \
     "git push origin master")
--- pullcmds.sh
+++ pullcmds.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Commands to be executed by the executecmds.sh script
-CMDS=("git status" \
-    "git pull origin master")
+CMDS=("git pull origin master")
