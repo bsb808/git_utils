b5af7006c311aac94d3ede9b168022f3abdd55c4
Bingham, Brian S <briansbingham@gmail.com>
2016-12-28 15:10:15 -0800

    working towards twist control

--- checkincmds.sh
+++ checkincmds.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-MSG="working towards twist control"
+MSG="Functional twist control with joystick and CLI commands"
 
 CMDS=("git commit -a -m '${MSG}' " \
     "git push origin master")
