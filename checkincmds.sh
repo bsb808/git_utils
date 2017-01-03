0d036539c23edf68ec3f0ac37ff521afff0242a3
Bingham, Brian S <briansbingham@gmail.com>
2017-01-03 15:13:26 -0800

    Functional version of navigation stack - found a UTM X/Y transposition

--- checkincmds.sh
+++ checkincmds.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-MSG="Working on navigation stack"
+MSG="Functional version of navigation stack - found a UTM X/Y transposition"
 
 CMDS=("git commit -a -m '${MSG}' " \
     "git push origin master")
