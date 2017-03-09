c39cba57230feaaa0981e406afb5a1b9dd451b8c
Bingham, Brian S <bsbingha@nps.edu>
2017-03-09 10:57:01 -0800

    adding bits for kingfisher pulls

---
+++ clone_from_laptop.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# get two lists of repos from repos.sh
+# also defines the catkin and working directories
+. ./repos.sh
+
+IP="192.168.131.211"
+USER="bsb"
+
+cd /home/frl/catkin_ws
+
+# Make sure to run "git daemon" on laptop and do 
+# touch git-daemon-export-ok
+# for each repo
+
+for REPO in "${ROSREPOS[@]}"
+do 
+    echo "--------${REPO}--------"
+    git clone ssh://${USER}@${IP}:catkin_ws/${REPO}
+done
+
+
+for REPO in "${OTHERREPOS[@]}"
+do 
+
+done
