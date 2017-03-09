b696bd21d241c8f012d6ec75ed42417d84d142d4
Bingham, Brian S <bsbingha@nps.edu>
2017-03-09 11:05:05 -0800

    adding bits for kingfisher pulls

--- clone_from_laptop.sh
+++ clone_from_laptop.sh
@@ -7,20 +7,21 @@
 IP="192.168.131.221"
 USER="bsb"
 
-cd /home/frl/catkin_ws
+
 
 # Make sure to run "git daemon" on laptop and do 
 # touch git-daemon-export-ok
 # for each repo
-
+cd /home/frl/catkin_ws/src
 for REPO in "${ROSREPOS[@]}"
 do 
     echo "--------${REPO}--------"
-    git clone ssh://${USER}@${IP}/home/bsb/catkin_ws/src/${REPO}
+    git clone ssh://${USER}@${IP}/home/${USER}/catkin_ws/src/${REPO}
 done
 
-
+cd /home/frl/WorkingCopies
 for REPO in "${OTHERREPOS[@]}"
 do 
- echo "hi"
+   echo "--------${REPO}--------"
+   git clone ssh://${USER}@${IP}/home/${USER}/WorkingCopies/${REPO}
 done
