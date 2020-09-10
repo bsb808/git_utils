c62629ba7362f1dbd2a0c29c14b8888b114840dc
Brian Bingham <briansbingham@gmail.com>
2020-09-09 17:48:28 -0700

    generalizing recursive

--- pull_recursive.sh
+++ pull_recursive.sh
@@ -1,7 +1,16 @@
+#!/bin/bash
 
+# Explicit
 WS="${HOME}/leanis_ws"
 
+# Implicit
+SRC=`dirname "${PWD}"`
+WS=`dirname "${SRC}"`
+
+echo "Pulling all src directories in <${WS}>"
+
 for dir in ${WS}/src/*/; do
+    echo "----------------------------------------"
     echo $dir
     cd ${dir}
     git pull
